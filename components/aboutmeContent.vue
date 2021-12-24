e1d702cdb528e12eed878a0fe15ea8b467852ea5
Dizuite <blog.christianmarin@gmail.com>
2021-12-24 20:16:34 +0100

    [ADD] Started creating aboutmeContent component

---
+++ components/aboutmeContent.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <main class="aboutme-content">
+    <div class="aboutme-content__bloc-image">
+
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  props: ['data'],
+}
+</script>
+
+<style lang="css" scoped>
+.aboutme-content {
+  padding: 64px 8vw;
+}
+
+.aboutme-content__bloc-image {
+  height: 256px;
+  width: 256px;
+  background-position: center;
+  background-size: cover;
+  background-color: lightgrey;
+}
+</style>
